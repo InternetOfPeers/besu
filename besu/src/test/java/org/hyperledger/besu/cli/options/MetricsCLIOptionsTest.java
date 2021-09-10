68ac2f5a9df65915eb98912c38271e0fd7739038
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2021-09-10 13:47:06 +0200

    Metrics idle timeout is now configurable

--- besu/src/test/java/org/hyperledger/besu/cli/options/MetricsCLIOptionsTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/options/MetricsCLIOptionsTest.java
@@ -27,8 +27,9 @@ public class MetricsCLIOptionsTest
 
   @Override
   MetricsConfiguration.Builder createCustomizedDomainObject() {
-    return MetricsConfiguration.builder()
-        .timersEnabled(!MetricsConfiguration.DEFAULT_TIMERS_ENABLED);
+    return MetricsConfiguration.builder();
+    // .timersEnabled(!MetricsConfiguration.DEFAULT_METRICS_TIMERS_ENABLED)
+    // .idleTimeout(MetricsConfiguration.DEFAULT_METRICS_IDLE_TIMEOUT);
   }
 
   @Override
